0caf16272b62c6488f1ca0872d252506109c2ae1
Juunini <juuni.ni.i@gmail.com>
2023-07-25 20:14:00 +0900

    feat: map equal

--- query/mapEqual.go
+++ query/mapEqual.go
@@ -4,95 +4,59 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/cloudmatelabs/gorm-gqlgen-relay/utils"
 	"gorm.io/gorm"
 )
 
-var (
-	POSTGRES_FLATTEN_OPTIONS = utils.FlattenOptions{
-		Delimiter: "->",
-		ListStart: "->",
-		ListEnd:   "->",
-		KeyWrap:   "'",
-	}
-	SQL_FLATTEN_OPTIONS = utils.FlattenOptions{
-		Delimiter: ".",
-		ListStart: "[",
-		ListEnd:   "]",
-	}
-)
-
-func MapEqual(field string, input *map[string]interface{}) func(db *gorm.DB) *gorm.DB {
-	if input == nil {
-		return self()
-	}
-
+func MapEqual(field string, key string, value any) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
-		var scope func(db *gorm.DB) *gorm.DB
-
 		switch db.Dialector.Name() {
 		case "postgres":
-			scope = gatherScopes("(%s->%s)::jsonb = '?'", field, input, "", POSTGRES_FLATTEN_OPTIONS)
+			db = db.Where(fmt.Sprintf("(%s->%s)::jsonb = '?'", field, key), value)
 		case "sqlserver":
-			scope = gatherScopes("JSON_VALUE(%s, '%s') = '?'", field, input, "$.", SQL_FLATTEN_OPTIONS)
+			db = db.Where(fmt.Sprintf("JSON_VALUE(%s, '%s') = ?", field, key), value)
 		default:
-			scope = gatherScopes("JSON_EXTRACT(%s, '%s') = '?'", field, input, "$.", SQL_FLATTEN_OPTIONS)
+			db = db.Where(fmt.Sprintf("JSON_EXTRACT(%s, '%s') = ?", field, key), value)
 		}
 
-		return db.Scopes(scope)
+		return db
 	}
 }
 
-func MapNotEqual(field string, input *map[string]interface{}) func(db *gorm.DB) *gorm.DB {
-	if input == nil {
-		return self()
-	}
-
+func MapNotEqual(field string, key string, value any) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
-		var scope func(db *gorm.DB) *gorm.DB
-
 		switch db.Dialector.Name() {
 		case "postgres":
-			scope = gatherScopes("(%s->%s)::jsonb != '?'", field, input, "", POSTGRES_FLATTEN_OPTIONS)
+			db = db.Where(fmt.Sprintf("(%s->%s)::jsonb != '?'", field, key), value)
 		case "sqlserver":
-			scope = gatherScopes("JSON_VALUE(%s, '%s') != '?'", field, input, "$.", SQL_FLATTEN_OPTIONS)
+			db = db.Where(fmt.Sprintf("JSON_VALUE(%s, '%s') != ?", field, key), value)
 		default:
-			scope = gatherScopes("JSON_EXTRACT(%s, '%s') != '?'", field, input, "$.", SQL_FLATTEN_OPTIONS)
+			db = db.Where(fmt.Sprintf("JSON_EXTRACT(%s, '%s') != ?", field, key), value)
 		}
 
-		return db.Scopes(scope)
+		return db
 	}
 }
 
-func MapEqualFold(field string, input *map[string]interface{}) func(db *gorm.DB) *gorm.DB {
-	if input == nil {
-		return self()
-	}
-
+func MapEqualFold(field string, key string, value string) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
-		var scope func(db *gorm.DB) *gorm.DB
-
 		switch db.Dialector.Name() {
 		case "postgres":
-			scope = gatherScopes("LOWER((%s->%s)::text) = '?'", field, input, "", POSTGRES_FLATTEN_OPTIONS)
+			db = db.Where(
+				fmt.Sprintf("LOWER((%s->%s)::text) = '?'", field, key),
+				strings.ToLower(value),
+			)
 		case "sqlserver":
-			scope = gatherScopes("LOWER(JSON_VALUE(%s, '%s')) = '?'", field, input, "$.", SQL_FLATTEN_OPTIONS)
+			db = db.Where(
+				fmt.Sprintf("LOWER(JSON_VALUE(%s, '%s')) = ?", field, key),
+				strings.ToLower(value),
+			)
 		default:
-			scope = gatherScopes("LOWER(JSON_EXTRACT(%s, '%s')) = '?'", field, input, "$.", SQL_FLATTEN_OPTIONS)
+			db = db.Where(
+				fmt.Sprintf("LOWER(JSON_EXTRACT(%s, '%s')) = ?", field, key),
+				strings.ToLower(value),
+			)
 		}
 
-		return db.Scopes(scope)
-	}
-}
-
-func gatherScopes(query string, field string, input *map[string]interface{}, prefix string, options utils.FlattenOptions) func(db *gorm.DB) *gorm.DB {
-	var value map[string]interface{}
-	utils.Flatten(prefix, *input, value, options)
-
-	return func(db *gorm.DB) *gorm.DB {
-		for k, v := range value {
-			db = db.Where(fmt.Sprintf(query, field, k), strings.ToLower(v.(string)))
-		}
 		return db
 	}
 }
