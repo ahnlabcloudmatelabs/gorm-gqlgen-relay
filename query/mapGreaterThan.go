1364d3a6209d51931e4b25e14b9665f9973115c0
Juunini <juuni.ni.i@gmail.com>
2023-07-25 20:30:14 +0900

    feat: map gt and lt

---
+++ query/mapGreaterThan.go
@@ -0,0 +1,35 @@
+package query
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+func MapGreaterThan(field, key string, value any) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		switch db.Dialector.Name() {
+		case "postgres":
+			db = db.Where(fmt.Sprintf("(%s->%s)::jsonb > '?'", field, key), value)
+		case "sqlserver":
+			db = db.Where(fmt.Sprintf("JSON_VALUE(%s, '%s') > ?", field, key), value)
+		default:
+			db = db.Where(fmt.Sprintf("JSON_EXTRACT(%s, '%s') > ?", field, key), value)
+		}
+		return db
+	}
+}
+
+func MapGreaterThanOrEqual(field, key string, value any) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		switch db.Dialector.Name() {
+		case "postgres":
+			db = db.Where(fmt.Sprintf("(%s->%s)::jsonb >= '?'", field, key), value)
+		case "sqlserver":
+			db = db.Where(fmt.Sprintf("JSON_VALUE(%s, '%s') >= ?", field, key), value)
+		default:
+			db = db.Where(fmt.Sprintf("JSON_EXTRACT(%s, '%s') >= ?", field, key), value)
+		}
+		return db
+	}
+}
