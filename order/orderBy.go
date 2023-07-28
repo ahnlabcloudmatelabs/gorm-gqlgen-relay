1ea515a837b39d73877b8c886af4b463ca94bb2c
Juunini <juuni.ni.i@gmail.com>
2023-07-28 20:39:28 +0900

    feat: orderBy

---
+++ order/orderBy.go
@@ -0,0 +1,39 @@
+package order
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+func OrderBy(db *gorm.DB, orderBy any, tableName string) *gorm.DB {
+	filter := parseOrderBy(orderBy)
+	if len(filter) == 0 {
+		return db
+	}
+
+	prefix := tablePrefix(tableName)
+
+	for _, v := range filter {
+		db = db.Order(fmt.Sprintf("%s%s %s", prefix, v["field"], v["direction"]))
+	}
+
+	return db
+}
+
+func parseOrderBy(orderBy any) []map[string]any {
+	filter := []map[string]any{}
+	data, _ := json.Marshal(orderBy)
+	json.Unmarshal(data, &filter)
+
+	return filter
+}
+
+func tablePrefix(tableName string) string {
+	if tableName == "" {
+		return ""
+	}
+
+	return tableName + "."
+}
