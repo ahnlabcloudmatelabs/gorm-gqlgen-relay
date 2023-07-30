7830793da90e16c31dd32aaffbe56800fcb66615
Juunini <juuni.ni.i@gmail.com>
2023-07-30 11:49:58 +0900

    fix: order returns string array

--- order/order.go
+++ order/order.go
@@ -2,10 +2,10 @@ package order
 
 import "github.com/cloudmatelabs/gorm-gqlgen-relay/utils"
 
-func By(input any, reverse bool) (string, error) {
+func By(input any, reverse bool) ([]string, error) {
 	filter, err := utils.ConvertToMap(input)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	return traverse(filter, reverse), nil
--- order/traverse.go
+++ order/traverse.go
@@ -2,12 +2,12 @@ package order
 
 import "github.com/cloudmatelabs/gorm-gqlgen-relay/utils"
 
-func traverse(filter map[string]any, reverse bool) (query string) {
+func traverse(filter map[string]any, reverse bool) (query []string) {
 	for field, direction := range filter {
 		if reverse {
 			direction = utils.ReverseDirection(direction.(string))
 		}
-		query = utils.AppendOrder(query, field, direction.(string))
+		query = append(query, field+" "+direction.(string))
 	}
 	return
 }
