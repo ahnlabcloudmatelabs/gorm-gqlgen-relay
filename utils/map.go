2a50ca2d30d46015074018a75b8b7216bc010f5c
Juunini <juuni.ni.i@gmail.com>
2023-07-30 11:55:59 +0900

    feat: map to base64 util

--- utils/map.go
+++ utils/map.go
@@ -1,6 +1,9 @@
 package utils
 
-import "encoding/json"
+import (
+	"encoding/base64"
+	"encoding/json"
+)
 
 func ConvertToMap(src any) (map[string]any, error) {
 	data, err := json.Marshal(src)
@@ -11,4 +14,13 @@ func ConvertToMap(src any) (map[string]any, error) {
 	dest := make(map[string]any)
 	err = json.Unmarshal(data, &dest)
 	return dest, err
+}
+
+func MapToBase64(src map[string]any) (string, error) {
+	data, err := json.Marshal(src)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.StdEncoding.EncodeToString(data), nil
 }
