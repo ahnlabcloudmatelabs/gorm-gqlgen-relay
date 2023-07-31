70815142382cc11e17382bad7634a449c8eddaf2
Juunini <juuni.ni.i@gmail.com>
2023-08-01 00:00:22 +0900

    feat: filter generate

--- model/generated.go
+++ model/generated.go
@@ -1,5 +1,15 @@
 package model
 
-import "github.com/cloudmatelabs/gorm-gqlgen-relay/relay"
+import (
+	"github.com/cloudmatelabs/gorm-gqlgen-relay/filters"
+	"github.com/cloudmatelabs/gorm-gqlgen-relay/relay"
+)
 
 type PageInfo = relay.PageInfo
+
+type IDFilter = filters.IDFilter
+type IntFilter = filters.IntFilter
+type MapFilter = filters.MapFilter
+type StringFilter = filters.StringFilter
+type TimeFilter = filters.TimeFilter
+type UUIDFilter = filters.UUIDFilter
