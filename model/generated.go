2bd0d39a5590cede9a53fa64e314453990d15cbd
Juunini <juuni.ni.i@gmail.com>
2023-08-01 00:09:00 +0900

    fix: remove filter generator

--- model/generated.go
+++ model/generated.go
@@ -1,19 +1,7 @@
 package model
 
 import (
-	"github.com/cloudmatelabs/gorm-gqlgen-relay/filters"
 	"github.com/cloudmatelabs/gorm-gqlgen-relay/relay"
 )
 
 type PageInfo = relay.PageInfo
-
-type StringFilter = filters.StringFilter
-type IntFilter = filters.IntFilter
-type FloatFilter = filters.FloatFilter
-type BooleanFilter = filters.BooleanFilter
-type TimeFilter = filters.TimeFilter
-type UUIDFilter = filters.UUIDFilter
-type IDFilter = filters.IDFilter
-type MapFilter = filters.MapFilter
-type MapEntry = filters.MapEntry
-type MapInEntry = filters.MapInEntry
