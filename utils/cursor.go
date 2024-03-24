7fd959a9d26e87f7d56c1236643ac03fbd02cbd6
captchanjack <53301102+captchanjack@users.noreply.github.com>
2024-03-24 19:18:04 +1100

    fix: Catch error associated to incorrect before/after cursor (#13)

--- utils/cursor.go
+++ utils/cursor.go
@@ -18,3 +18,25 @@ func ParseCursor(src string) (map[string]any, error) {
 
 	return cursor, nil
 }
+
+func SameKeys(map1, map2 map[string]interface{}) bool {
+	if len(map1) != len(map2) {
+		return false
+	}
+
+	for key := range map1 {
+		if _, ok := map2[key]; !ok {
+			return false
+		}
+	}
+
+	return true
+}
+
+func GetMapKeys(inputMap map[string]interface{}) []string {
+	keys := make([]string, 0, len(inputMap))
+	for key := range inputMap {
+		keys = append(keys, key)
+	}
+	return keys
+}
